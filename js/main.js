45c7a7a04ffc628fd202229ea360144075a91cec
HijenHek <k.hijen@gmail.com>
2021-01-11 22:37:41 +0100

    skills + nav-active

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 
 
-ScrollReveal().reveal('.intro', { delay: 300 , interval: 16, reset: true  });
-ScrollReveal().reveal('.image', { delay: 450 , interval: 16, reset: true  });
+ScrollReveal().reveal('.intro', { delay: 100 , interval: 16, reset: true  });
+ScrollReveal().reveal('.image', { delay: 200 , interval: 16, reset: true  });
 
 
-ScrollReveal().reveal('.about .heading', { delay: 300 , interval: 16, reset: true  });
-ScrollReveal().reveal('.about .content', { delay: 450 , interval: 16, reset: true  });
+ScrollReveal().reveal('.about ', { delay: 150 , interval: 16, reset: true  });
 
-ScrollReveal().reveal('.about .action', { delay: 650 , interval: 16, reset: true  });
+ScrollReveal().reveal('.skills .card ', { delay: 150 , interval: 50, reset: true  });
+
 
 var age = document.querySelector('#age') ;
 var date = new Date() ;
 age.innerHTML ="" + (date.getFullYear() - 1996) ;
+
+
+var nav = document.querySelector('.nav');
+
+nav.addEventListener('click' , function (event) { 
+    var active = document.querySelector('.nav-active');
+    if(event.target != active) {
+        event.target.classList += " nav-active";
+        active.classList = "nav-item";
+    }
+ });
