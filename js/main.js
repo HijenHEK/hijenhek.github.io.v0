75b5591c24676bfed79bea8472f843b790560330
HijenHek <k.hijen@gmail.com>
2021-01-11 20:16:40 +0100

    scroll reveal + links

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var home = document.querySelector('.section.home') ;
+ScrollReveal().reveal('.brand', { delay: 400 , interval: 150});
+ScrollReveal().reveal('.nav-item', { delay: 500 , interval: 150});
+
+ScrollReveal().reveal('.intro', { delay: 400 , interval: 16, reset: true  });
+ScrollReveal().reveal('.image', { delay: 600 , interval: 16, reset: true  });
