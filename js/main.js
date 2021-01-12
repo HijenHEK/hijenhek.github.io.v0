04128d3f0ede233ecda8eb0c4403b8d48033b96c
HijenHek <k.hijen@gmail.com>
2021-01-12 14:41:38 +0100

    contact + more on wokr

--- js/main.js
+++ js/main.js
@@ -1,25 +1,49 @@
 
 
-ScrollReveal().reveal('.intro', { delay: 100 , interval: 16, reset: true  });
-ScrollReveal().reveal('.image', { delay: 200 , interval: 16, reset: true  });
+ScrollReveal().reveal('.intro', { delay: 150 , interval: 50, reset: true  });
+ScrollReveal().reveal('.image', { delay: 200 , interval: 50, reset: true  });
 
 
-ScrollReveal().reveal('.about ', { delay: 150 , interval: 16, reset: true  });
+ScrollReveal().reveal('.about ', { delay: 200 , interval: 50, reset: true  });
 
-ScrollReveal().reveal('.skills .card ', { delay: 150 , interval: 50, reset: true  });
-
-
-var age = document.querySelector('#age') ;
-var date = new Date() ;
-age.innerHTML ="" + (date.getFullYear() - 1996) ;
+ScrollReveal().reveal('.skills .card ', { delay: 200 , interval: 50, reset: true  });
+ScrollReveal().reveal('.work .card ', { delay: 200 , interval: 50, reset: true  });
+ScrollReveal().reveal('.contact , .contact .group ', { delay: 200 , interval: 50, reset: true  });
 
 
-var nav = document.querySelector('.nav');
 
-nav.addEventListener('click' , function (event) { 
-    var active = document.querySelector('.nav-active');
-    if(event.target != active) {
-        event.target.classList += " nav-active";
-        active.classList = "nav-item";
-    }
- });
+
+
+
+
+ window.onload = function() {
+
+    var age = document.querySelector('#age') ;
+    var date = new Date() ;
+    age.innerHTML ="" + (date.getFullYear() - 1996) ;
+
+
+    var nav = document.querySelector('.nav');
+
+    nav.addEventListener('click' , function (event) { 
+        var active = document.querySelector('.nav-active');
+        if(event.target != active) {
+            event.target.classList += " nav-active";
+            active.classList = "nav-item";
+        }
+    });
+
+    // document.getElementById('contact-form').addEventListener('submit', function(event) {
+    //     event.preventDefault();
+    //     // generate a five digit number for the contact_number variable
+    //     this.contact_number.value = Math.random() * 100000 | 0;
+    //     // these IDs from the previous steps
+        
+    //     emailjs.sendForm('service_rbgzsa4',  'template_cor3guk' , this , 'user_4LBlWTZucC4ejcWgv1UPF')
+    //         .then(function() {
+    //             console.log('SUCCESS!');
+    //         }, function(error) {
+    //             console.log('FAILED...', error);
+    //         });
+    // });
+}
